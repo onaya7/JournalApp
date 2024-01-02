688d98418ccd6241cbfc392a5558018ce700016d
samuel <samuelayano7@gmail.com>
2024-01-02 17:52:14 +0100

    Refactor import statements in main.dart and delete HomeScreen class

---
+++ lib/data/journal_edit.dart
@@ -0,0 +1,9 @@
+import 'package:journalapp/data/journal.dart';
+
+class JournalEdit {
+  String action;
+  Journal journal;
+  JournalEdit({required this.action, required this.journal});
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:journalapp/screens/home_screen.dart';
+import 'package:journalapp/screens/home/home_screen.dart';
 import 'package:journalapp/utils/colors.dart';
 
 void main() {
